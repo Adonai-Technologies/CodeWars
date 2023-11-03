48a8c679c1768e024c581434ddce592c2155e8f6
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-11-03 18:18:30 +0100

    //   An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia). //   Note: anagrams are case insensitive //   Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.

--- main.js
+++ main.js
@@ -30,3 +30,22 @@ function removeUrlAnchor(url) {
   console.log(removeUrlAnchor(url1));  // Output: "www.codewars.com"
   console.log(removeUrlAnchor(url2));  // Output: "www.codewars.com?page=1"
   
+
+
+//   An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+//   Note: anagrams are case insensitive
+//   Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+  function areAnagrams(str1, str2) {
+    // Remove spaces and convert to lowercase
+    const cleanStr1 = str1.replace(/\s+/g, '').toLowerCase();
+    const cleanStr2 = str2.replace(/\s+/g, '').toLowerCase();
+  
+    // Sort the characters and compare the resulting strings
+    return cleanStr1.split('').sort().join('') === cleanStr2.split('').sort().join('');
+  }
+  
+  // Example usage:
+  console.log(areAnagrams("listen", "silent")); // true
+  console.log(areAnagrams("hello", "world"));   // false
+  
--- main.rb
+++ main.rb
@@ -20,3 +20,23 @@ def remove_url_anchor(url)
   puts remove_url_anchor(url1)  # Output: "www.codewars.com"
   puts remove_url_anchor(url2)  # Output: "www.codewars.com?page=1"
   
+  
+# An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+# Note: anagrams are case insensitive
+# Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+  
+  
+  def are_anagrams(str1, str2)
+    # Remove spaces and convert to lowercase
+    clean_str1 = str1.gsub(/\s+/, '').downcase
+    clean_str2 = str2.gsub(/\s+/, '').downcase
+  
+    # Sort the characters and compare the resulting strings
+    clean_str1.chars.sort == clean_str2.chars.sort
+  end
+  
+  # Example usage:
+  puts are_anagrams("listen", "silent") # true
+  puts are_anagrams("hello", "world")   # false
+  
