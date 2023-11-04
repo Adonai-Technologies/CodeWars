a76e451e8e67384a6138d5c58df178c3af3dcb21
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-11-04 11:08:51 +0100

    5 without numbers !!

---
+++ 5 without numbers !!.js
@@ -0,0 +1,6 @@
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+function unusualFive() {
+    return 'Bible'.length
+  }
