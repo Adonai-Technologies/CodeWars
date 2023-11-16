63c241f62aaa375b4fb78777927ea1b6a0e2cefa
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-11-16 11:39:48 +0100

    List Filtering

---
+++ List Filtering.js
@@ -0,0 +1,6 @@
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+     return l.filter(item => typeof item === 'number' && item >= 0);
+  }
