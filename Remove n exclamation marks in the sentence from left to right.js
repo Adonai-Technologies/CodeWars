b0379cc9e71b1426017cc8165cd70b5c59c513fb
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-12-04 14:30:29 +0100

    Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right

---
+++ Remove n exclamation marks in the sentence from left to right.js
@@ -0,0 +1,28 @@
+// Description:
+// Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+// Examples
+// remove("Hi!",1) === "Hi"
+// remove("Hi!",100) === "Hi"
+// remove("Hi!!!",1) === "Hi!!"
+// remove("Hi!!!",100) === "Hi"
+// remove("!Hi",1) === "Hi"
+// remove("!Hi!",1) === "Hi!"
+// remove("!Hi!",100) === "Hi"
+// remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+
+
+function remove(str,n){
+    //coding and coding....
+     return str.replace(/!/g, function (m0) {
+      if (n > 0) {
+        n--;
+        return '';
+      }
+      return m0;
+    });
+    
+  }
