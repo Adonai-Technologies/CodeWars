223b62a16432dc4bc310b5532b032d5e6d052988
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2023-12-22 15:16:01 +0100

    Beginner - Lost Without a Map

---
+++ Lost Without a Map.js
@@ -0,0 +1,10 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(arr){
+    return arr.map(num => num * 2);
+  }
+   
