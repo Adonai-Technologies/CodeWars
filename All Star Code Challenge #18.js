b864588286f83edb11bb67133dc137f538f2a5ff
Akua Konadu <130577072+ADO-Tech-Web@users.noreply.github.com>
2024-01-17 17:27:58 +0100

    All Star Code Challenge #18

---
+++ All Star Code Challenge #18.js
@@ -0,0 +1,21 @@
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+// str_count("Hello", 'o'); // returns 1
+// str_count("Hello", 'l'); // returns 2
+// str_count("", 'z'); // returns 0
+// Notes
+// The first argument can be an empty string
+// In languages with no distinct character data type, the second argument will be a string of length 1
+
+function strCount(str, letter){  
+    //code here
+     // Use split to convert the string into an array of characters
+    // Use filter to keep only the characters that match the specified character
+    // Return the length of the filtered array, which represents the count of occurrences
+    return str.split('').filter(c => c === letter).length;
+  }
